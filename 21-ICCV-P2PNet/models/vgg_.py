2c3549f794705ccbaaf0817727495688b91f2d39
mingilee <leemingi0906@gm.gist.ac.kr>
2026-01-15 18:02:33 +0900

    +

--- 21-ICCV-P2PNet/models/vgg_.py
+++ 21-ICCV-P2PNet/models/vgg_.py
@@ -102,7 +102,11 @@ def _vgg(arch, cfg, batch_norm, pretrained, progress, sync=False, **kwargs):
         kwargs['init_weights'] = False
     model = VGG(make_layers(cfgs[cfg], batch_norm=batch_norm, sync=sync), **kwargs)
     if pretrained:
-        state_dict = torch.load(model_paths[arch])
+        state_dict = load_state_dict_from_url(
+            model_urls[arch],
+            progress=progress
+        )
+        #state_dict = torch.load(model_paths[arch])
         model.load_state_dict(state_dict)
     return model
 
